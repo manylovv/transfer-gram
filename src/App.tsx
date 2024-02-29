977681e1a9868aed6944f3b50fd40e4f7603e8e6
manylovv <yellowmanylov@gmail.com>
2024-02-29 21:01:25 +0600

    feat: disable inputs when user isn't connected

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,24 @@ import {
   TonConnectButton,
   useTonAddress,
   useTonConnectUI,
+  useTonWallet,
 } from '@tonconnect/ui-react';
 import { getTransfer } from 'gram20-sdk/v2';
-import { FormEvent, useState } from 'react';
+import {
+  ElementRef,
+  FormEvent,
+  Fragment,
+  Ref,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from 'react';
 
 function App() {
   const [tonConnectUI] = useTonConnectUI();
   const address = useTonAddress();
+  const isConnected = !!useTonWallet();
 
   const [recipient, setRecipient] = useState('');
   const [amount, setAmount] = useState('');
@@ -40,45 +51,56 @@ function App() {
     setTick('');
   };
 
+  const firstInputRef = useRef<ElementRef<'input'>>(null);
+
+  useEffect(() => {
+    if (isConnected) {
+      firstInputRef.current?.focus();
+    }
+  }, [isConnected]);
+
   return (
     <div>
-      <div className="flex justify-end p-10">
+      <div className="flex justify-end absolute w-full p-10">
         <TonConnectButton />
       </div>
-      <div className="flex justify-center p-10">
-        <div className="flex gap-2">
-          <form
-            className="flex flex-col gap-2 w-[400px]"
-            onSubmit={(e) => e.preventDefault()}
-          >
-            <input
-              placeholder="Recipient"
-              value={recipient}
-              onChange={(e) => setRecipient(e.target.value)}
-              className="border px-4 p-2 rounded-full "
-            />
-            <input
-              placeholder="Amount"
-              value={amount}
-              onChange={(e) => setAmount(e.target.value)}
-              className="border px-4 p-2 rounded-full"
-            />
-            <input
-              placeholder="Tick"
-              value={tick}
-              onChange={(e) => setTick(e.target.value)}
-              className="border px-4 p-2 rounded-full"
-            />
+      <div className="flex justify-center items-center pb-20 h-screen p-10">
+        <form
+          className="flex flex-col gap-2 p-10 w-[400px]"
+          onSubmit={(e) => e.preventDefault()}
+        >
+          <input
+            ref={firstInputRef}
+            placeholder="Recipient"
+            value={recipient}
+            onChange={(e) => setRecipient(e.target.value)}
+            className="border px-4 p-2 rounded-full disabled:pointer-events-none disabled:opacity-70 focus:outline-sky-500"
+            disabled={!isConnected}
+          />
+          <input
+            placeholder="Amount"
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+            className="border px-4 focus:outline-sky-500 p-2 rounded-full disabled:pointer-events-none disabled:opacity-70"
+            disabled={!isConnected}
+          />
+          <input
+            placeholder="Tick"
+            value={tick}
+            onChange={(e) => setTick(e.target.value)}
+            className="border px-4 p-2 focus:outline-sky-500 rounded-full disabled:pointer-events-none disabled:opacity-70"
+            disabled={!isConnected}
+          />
 
-            <button
-              type="submit"
-              onClick={sendTx}
-              className="border px-4 p-2 rounded-full bg-sky-500 text-white"
-            >
-              Send Tx
-            </button>
-          </form>
-        </div>
+          <button
+            type="submit"
+            onClick={sendTx}
+            className="border px-4 p-2 rounded-full disabled:bg-sky-300 bg-sky-500 text-white"
+            disabled={!isConnected}
+          >
+            Send Tx
+          </button>
+        </form>
       </div>
     </div>
   );
