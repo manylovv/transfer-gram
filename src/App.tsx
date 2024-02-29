71052eeec329aa76dc21f15c010bda3864ad2a94
manylovv <yellowmanylov@gmail.com>
2024-02-29 19:11:54 +0600

    feat: add transfer

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,84 @@
-import { TonConnectButton, useTonConnectUI } from '@tonconnect/ui-react';
-// import { beginCell } from 'ton-core';
-import TonWeb from 'tonweb';
+import {
+  TonConnectButton,
+  useTonAddress,
+  useTonConnectUI,
+} from '@tonconnect/ui-react';
+import { getTransfer } from 'gram20-sdk/v2';
+import { FormEvent, useState } from 'react';
 
 function App() {
   const [tonConnectUI] = useTonConnectUI();
+  const address = useTonAddress();
 
-  const sendMessage = async () => {
-    const a = new TonWeb.boc.Cell();
-    a.bits.writeUint(0, 32);
-    a.bits.writeString('TON Connect 2 tutorial!');
-    const payload = TonWeb.utils.bytesToBase64(await a.toBoc());
+  const [recipient, setRecipient] = useState('');
+  const [amount, setAmount] = useState('');
+  const [tick, setTick] = useState('');
 
-    const myTransaction = {
-      validUntil: Math.floor(Date.now() / 1000) + 60, // 60 sec
+  const sendTx = async (e: FormEvent) => {
+    e.preventDefault();
+
+    const { payload, address: tokenAddress } = await getTransfer({
+      address: address,
+      to: recipient,
+      amount: amount,
+      tick: tick,
+    });
+
+    await tonConnectUI.sendTransaction({
       messages: [
         {
-          address: 'EQDmnxDMhId6v1Ofg_h5KR5coWlFG6e86Ro3pc7Tq4CA0-Jn',
-          amount: '60000000',
+          address: tokenAddress,
+          amount: '8000000',
           payload: payload,
         },
       ],
-    };
+      validUntil: Math.floor(Date.now() / 1000) + 60,
+    });
 
-    await tonConnectUI.sendTransaction(myTransaction);
+    setRecipient('');
+    setAmount('');
+    setTick('');
   };
-  return (
-    <div className="flex justify-end p-20">
-      <div className="flex gap-2">
-        <button onClick={sendMessage} className="bg-gray-300 w-40 rounded-full">
-          Send message
-        </button>
 
+  return (
+    <div>
+      <div className="flex justify-end p-10">
         <TonConnectButton />
+      </div>
+      <div className="flex justify-center p-10">
+        <div className="flex gap-2">
+          <form
+            className="flex flex-col gap-2 w-[400px]"
+            onSubmit={(e) => e.preventDefault()}
+          >
+            <input
+              placeholder="Recipient"
+              value={recipient}
+              onChange={(e) => setRecipient(e.target.value)}
+              className="border px-4 p-2 rounded-full "
+            />
+            <input
+              placeholder="Amount"
+              value={amount}
+              onChange={(e) => setAmount(e.target.value)}
+              className="border px-4 p-2 rounded-full"
+            />
+            <input
+              placeholder="Tick"
+              value={tick}
+              onChange={(e) => setTick(e.target.value)}
+              className="border px-4 p-2 rounded-full"
+            />
+
+            <button
+              type="submit"
+              onClick={sendTx}
+              className="border px-4 p-2 rounded-full bg-sky-500 text-white"
+            >
+              Send Tx
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   );
